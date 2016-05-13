a54c98782e67f8d431da7ad7d6faabb3923d3cd1
Ruben2112 <rubenvanamelsvoort@gmail.com>
2016-05-13 14:44:49 +0200

    Added layout for login and register

--- Bourdrbij/app/src/main/java/com/thunderbuild/bourdrbij/LogInActivity.java
+++ Bourdrbij/app/src/main/java/com/thunderbuild/bourdrbij/LogInActivity.java
@@ -1,8 +1,11 @@
 package com.thunderbuild.bourdrbij;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInActivity extends Activity {
 
@@ -10,5 +13,14 @@ public class LogInActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        Button btRegister = (Button) findViewById(R.id.bt_register);
+        btRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LogInActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
