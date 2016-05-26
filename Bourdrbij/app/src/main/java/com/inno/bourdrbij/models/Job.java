efd8ee24528f42c559cd3914b141683821ea2fb1
Phyxel <cypher@live.nl>
2016-05-26 21:08:26 +0200

    Job class

--- Bourdrbij/app/src/main/java/com/inno/bourdrbij/models/Job.java
+++ Bourdrbij/app/src/main/java/com/inno/bourdrbij/models/Job.java
@@ -4,4 +4,55 @@ package com.inno.bourdrbij.models;
  * Created by sebas on 5/20/2016.
  */
 public class Job {
+
+    private int id;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void GetAll()
+    {
+
+    }
+
+    public void GetLast(int count)
+    {
+
+    }
+
+    public void NewJob(String username, String title, String Description, String reward)
+    {
+
+    }
+
+    public void DeleteJob(int id)
+    {
+
+    }
+
+    public void UpdateJob(int id, String description, String reward)
+    {
+
+    }
+
+    public void SearchJob(String searchCriteria)
+    {
+
+    }
+
+    public void AcceptJob(int id, String username)
+    {
+
+    }
+
+    public void RejectJob(int id, String username)
+    {
+
+    }
+
+    public void RecommendFriend(String username)
+    {
+
+    }
 }
