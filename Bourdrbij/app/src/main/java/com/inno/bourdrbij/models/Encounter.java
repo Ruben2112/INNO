f0fa413d5c23ea1866004c4ae15ddf7bd31502ac
Bazzzzz <sebastiaan.koch@hotmail.com>
2016-05-27 14:30:59 +0200

    Encounter Implementation complete

--- Bourdrbij/app/src/main/java/com/inno/bourdrbij/models/Encounter.java
+++ Bourdrbij/app/src/main/java/com/inno/bourdrbij/models/Encounter.java
@@ -2,7 +2,9 @@ package com.inno.bourdrbij.models;
 
 import com.inno.bourdrbij.servercommunication.HTTPRestClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.text.DateFormat;
@@ -16,6 +18,10 @@ import cz.msebera.android.httpclient.Header;
  * Created by sebas on 5/20/2016.
  */
 public class Encounter {
+    /**
+     * Id of profile.
+     */
+    private int id;
     /**
      * Profile encountered.
      */
@@ -59,7 +65,8 @@ public class Encounter {
         this.receiverConfirm = false;
     }
 
-    public Encounter (Profile receiverProfile, Profile senderProfile, Date date, int longitude, int latitude, int senderConfirm, int receiverConfirm) {
+    public Encounter (int id, Profile receiverProfile, Profile senderProfile, Date date, int longitude, int latitude, int senderConfirm, int receiverConfirm) {
+        this.id = id;
         this.receiverProfile = receiverProfile;
         this.senderProfile = senderProfile;
 
@@ -78,8 +85,68 @@ public class Encounter {
         }
     }
 
+    public int getId() {
+        return this.id;
+    }
+
+    public Profile getReceiverProfile() {
+        return this.receiverProfile;
+    }
+
+    public boolean getReceiverConfirm() {
+        return this.receiverConfirm;
+    }
+
+    public boolean getSenderConfirm() {
+        return this.senderConfirm;
+    }
+
+    public Profile getSenderProfile() {
+        return this.senderProfile;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public int getLongitude() {
+        return this.longitude;
+    }
+
+    public int getLatitude() {
+        return this.latitude;
+    }
+
+    /**
+     * Puts a Encounter in the database on the server.
+     */
     public void createEncounter() {
 
+        RequestParams params = new RequestParams();
+
+
+        params.put("receiverProfileId", this.getReceiverProfile().getId());
+        params.put("senderProfileId", this.getSenderProfile().getId());
+        params.put("date", this.getDate());
+        params.put("longitude", this.getLongitude());
+        params.put("latitude", this.getLatitude());
+        params.put("senderConfirm", this.getSenderConfirm());
+        params.put("receiverConfirm", this.getReceiverConfirm());
+
+        HTTPRestClient.post("encounter/", params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    if (response.getBoolean("success")) {
+                        System.out.println("Added an encounter to database.");
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
     }
 
     /**
@@ -113,7 +180,7 @@ public class Encounter {
                         Profile senderProfile = new Profile();
                         senderProfile = senderProfile.getProfile(profileIDSender);
 
-                        Encounter.receivedEncounter = new Encounter(receiverProfile, senderProfile, date, longitude, latitude, senderConfirm, receiverConfirm);
+                        Encounter.receivedEncounter = new Encounter(id, receiverProfile, senderProfile, date, longitude, latitude, senderConfirm, receiverConfirm);
                     }
 
                 } catch (Exception e) {
@@ -130,7 +197,10 @@ public class Encounter {
      * @param username Username of profile we want to invite.
      */
     public void invite(String username) {
-
+        this.receiverProfile = this.getProfile(username);
+        this.senderConfirm = true;
+        
+        this.updateEncounter();
     }
 
     /**
@@ -138,7 +208,10 @@ public class Encounter {
      * @param inviteID
      */
     public void accept(int inviteID) {
+        this.senderProfile.addFriend(this.receiverProfile.getId());
+        this.receiverConfirm = true;
 
+        this.updateEncounter();
     }
     // Is deze nodig?
     public void ignore(String username) {
@@ -150,6 +223,69 @@ public class Encounter {
      * @param inviteID
      */
     public void reject(int inviteID) {
+        // TODO: Remove Encounter?
+    }
 
+    /**
+     * Updates the Encounter in the database on the server.
+     * Update senderConfirm to true
+     * Update receiverConfirm to true or false.
+     */
+    private void updateEncounter() {
+
+        RequestParams params = new RequestParams();
+
+        params.put("receiverProfileId", this.getReceiverProfile().getId());
+        params.put("senderConfirm", this.getSenderConfirm());
+        params.put("receiverConfirm", this.getReceiverConfirm());
+        HTTPRestClient.put("updateEncounter/" + this.getId(), params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    if (response.getBoolean("success")) {
+                        System.out.println("Encounter updated.");
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+    }
+
+    /**
+     * Find profile with [@param} username in server database.
+     * @param username
+     * @return Profile that matches the [@param]
+     */
+    private Profile getProfile(String username) {
+        HTTPRestClient.get("retrievedProfile/" + username, null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    if (response.getBoolean("success")) {
+                        JSONObject json = response.getJSONObject("data");
+                        String username = json.getString("username");
+                        String profession = json.getString("profession");
+                        String address = json.getString("address");
+                        JSONArray intrestsJsonArray = json.getJSONArray("intrests");
+                        String[] intrests = new String[intrestsJsonArray.length()];
+                        for (int i = 0; i < intrestsJsonArray.length(); i++) {
+                            intrests[i] = intrestsJsonArray.getString(i);
+                        }
+                        int id = json.getInt("id");
+                        // String picturePath = json.getString("picture");
+
+                        Profile.retrievedProfile = new Profile(id, username, profession, address, intrests);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+        return Profile.retrievedProfile;
     }
 }
