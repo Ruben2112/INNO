dccb96597ede8ef1e8decd3f6cd44051f021ccec
Ruben2112 <rubenvanamelsvoort@gmail.com>
2016-05-27 09:57:00 +0200

    Commit for pull

--- Bourdrbij/app/src/main/java/com/inno/bourdrbij/MainActivity.java
+++ Bourdrbij/app/src/main/java/com/inno/bourdrbij/MainActivity.java
@@ -1,9 +1,5 @@
 package com.inno.bourdrbij;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
