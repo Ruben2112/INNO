f0a699d2f301065409ae881b2bab86dd7872b732
Phyxel <cypher@live.nl>
2016-06-03 09:58:52 +0200

    JobManager Fix

--- Bourdrbij/app/src/main/java/com/inno/bourdrbij/models/Event.java
+++ Bourdrbij/app/src/main/java/com/inno/bourdrbij/models/Event.java
@@ -1,7 +1,87 @@
 package com.inno.bourdrbij.models;
 
+import java.util.Date;
+
 /**
  * Created by sebas on 5/20/2016.
  */
 public class Event {
+
+    private int id;
+    private int ownerProfileID;
+    private String location;
+    private String name;
+    private String description;
+    private Date startTime;
+    private Date endTime;
+
+    public Event(int ownerProfileID, String location, String name, String description, Date startTime, Date endTime ) {
+        this.ownerProfileID = ownerProfileID;
+        this.location = location;
+        this.name = name;
+        this.description = description;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public void addFriends(int[] friendIDs)
+    {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOwnerProfileID() {
+        return ownerProfileID;
+    }
+
+    public void setOwnerProfileID(int ownerProfileID) {
+        this.ownerProfileID = ownerProfileID;
+    }
+
+    public String getAddressid() {
+        return location;
+    }
+
+    public void setAddressid(String location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
 }
